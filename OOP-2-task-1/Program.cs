661e4b088fc5bd7dac06b1c139fd01f65eb759b0
Batorshyn <batorshyndanil@gmail.com>
2026-09-11 09:47:25 +0300

    Add project files.

---
+++ OOP-2-task-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+string pattern = @"^[a-zA-Z][a-zA-Z0-9]{1,9}$";
+
+while (true)
+{
+    Console.Write("Введіть логін: ");
+    string login = Console.ReadLine();
+
+    if (Regex.IsMatch(login, pattern, RegexOptions.IgnoreCase))
+    {
+        Console.WriteLine("Логін підтверджено!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Некоректний логін (має бути 2-10 символів, тільки літери/цифри, перша - літера). \nСпробуйте ще раз.\n");
+    }
+}
